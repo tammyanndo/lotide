4f2a4f6a7278e07aa307a426cdb7f81120abd095
Tammy <tammyanndo@gmail.com>
2021-09-14 00:24:08 -0400

    Created assertEqual code

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("😎 Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("💩 Assertion Failed " + actual + " !== " + expected);
+  }
+
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(123, "abc");
