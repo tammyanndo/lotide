e93f87354e0858a00d4d399f38019bddfd94e580
Tammy <tammyanndo@gmail.com>
2021-09-20 15:54:51 -0400

    updated code for tail.js

--- tail.js
+++ tail.js
@@ -1,21 +1,5 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`😎 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`💩 Assertion Failed ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-const array = ["Hello", "Lighthouse", "Labs"]
-const result = tail(array);
-console.log(result);
-console.log(array);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
