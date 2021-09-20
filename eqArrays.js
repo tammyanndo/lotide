e0d1619156dc97e2afa9c80aabc6b1a276ced2c3
Tammy <tammyanndo@gmail.com>
2021-09-20 16:08:59 -0400

    updated eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😎 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`💩 Assertion Failed ${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString();  
+  return arr1.toString() === arr2.toString();
 }
-assertEqual(eqArrays([1, 3, 3], [1, 3, 3]), true);
 
+module.exports = eqArrays;
